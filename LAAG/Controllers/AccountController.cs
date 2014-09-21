c49da0477b7d96ba850ab91f6210c74195285da4
Jorge Rojas Aragonés <jeragones@gmail.com>
2014-09-20 21:29:18 -0600

    Reload

---
+++ LAAG/Controllers/AccountController.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using WebMatrix.WebData;
+using LAAG.Filters;
+using LAAG.Models;
+
+namespace LAAG.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class AccountController : Controller
+    {
+
+        DataDataContext db = new DataDataContext();
+
+        //
+        // GET: /Account/Login
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            Session["CurrentSession"] = null;
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            Session["CurrentSession"] = null;
+            if (ModelState.IsValid)
+            {
+                DataDataContext db = new DataDataContext();
+                IQueryable<Persona> persona = db.IsValidUser(model.UserName, model.Password);
+                if (persona!=null)
+                {
+                    foreach(var a in persona){
+                        Session["CurrentSession"] = a;
+                        return RedirectToLocal(returnUrl);
+                    }
+                    
+                }
+            }
+
+            // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
+            ModelState.AddModelError("", "El nombre de usuario o la contraseña especificados son incorrectos.");
+            return View(model);
+        }
+
+        //
+        // POST: /Account/LogOff
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            WebSecurity.Logout();
+            Session["CurrentSession"] = null;
+            return RedirectToAction("Login()", "Account");
+        }
+
+
+        //
+        // GET: /Account/Users
+
+        [AllowAnonymous]
+        public ActionResult Result()
+        {
+            var users = from x in db.Personas select x;
+            return View("Result",users);
+        }
+
+        //
+        // GET: /Account/ListaUsuarios
+
+        [AllowAnonymous]
+        public ActionResult Search()
+        {
+            return View();
+        }
+        
+        //
+        // GET: /Account/Register
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/Register
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                // Intento de registrar al usuario
+                try
+                {
+                    //WebSecurity.CreateUserAndAccount(model.UserName, model.Password);
+                    //WebSecurity.Login(model.UserName, model.Password);
+                    //return RedirectToAction("Index", "Home");
+                    String n = model.Nombre;
+                    Persona p = new Persona();
+                    p.Nombre = model.Nombre;
+                    String[] ape = model.Apellidos.Split(' ');
+                    p.Apellido1 = ape[0];
+                    if (ape.Length > 1) 
+                    {
+                        p.Apellido2 = ape[1];
+                    }
+                    p.Correo = model.Correo;
+                    p.Clave = "12345";
+                    p.Estado = 1;
+                    p.Tipo = 1;
+                    p.NombreUsuario = model.Usuario;
+
+                    db.Personas.InsertOnSubmit(p);
+                    db.SubmitChanges();
+
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("", ErrorCodeToString(e.StatusCode));
+                }
+            }
+
+            // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
+            return View(model);
+        }
+
+        //
+        // POST: /Account/Disassociate
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Disassociate(string provider, string providerUserId)
+        {
+            string ownerAccount = OAuthWebSecurity.GetUserName(provider, providerUserId);
+            ManageMessageId? message = null;
+
+            // Desasociar la cuenta solo si el usuario que ha iniciado sesión es el propietario
+            if (ownerAccount == User.Identity.Name)
+            {
+                // Usar una transacción para evitar que el usuario elimine su última credencial de inicio de sesión
+                using (var scope = new TransactionScope(TransactionScopeOption.Required, new TransactionOptions { IsolationLevel = IsolationLevel.Serializable }))
+                {
+                    bool hasLocalAccount = OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
+                    if (hasLocalAccount || OAuthWebSecurity.GetAccountsFromUserName(User.Identity.Name).Count > 1)
+                    {
+                        OAuthWebSecurity.DeleteAccount(provider, providerUserId);
+                        scope.Complete();
+                        message = ManageMessageId.RemoveLoginSuccess;
+                    }
+                }
+            }
+
+            return RedirectToAction("Manage", new { Message = message });
+        }
+
+        //
+        // GET: /Account/Manage
+
+        public ActionResult Manage(ManageMessageId? message)
+        {
+            ViewBag.StatusMessage =
+                message == ManageMessageId.ChangePasswordSuccess ? "La contraseña se ha cambiado."
+                : message == ManageMessageId.SetPasswordSuccess ? "Su contraseña se ha establecido."
+                : message == ManageMessageId.RemoveLoginSuccess ? "El inicio de sesión externo se ha quitado."
+                : "";
+            ViewBag.HasLocalPassword = OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
+            ViewBag.ReturnUrl = Url.Action("Manage");
+            return View();
+        }
+
+        //
+        // POST: /Account/Manage
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Manage(LocalPasswordModel model)
+        {
+            bool hasLocalAccount = OAuthWebSecurity.HasLocalAccount(WebSecurity.GetUserId(User.Identity.Name));
+            ViewBag.HasLocalPassword = hasLocalAccount;
+            ViewBag.ReturnUrl = Url.Action("Manage");
+            if (hasLocalAccount)
+            {
+                if (ModelState.IsValid)
+                {
+                    // ChangePassword iniciará una excepción en lugar de devolver false en determinados escenarios de error.
+                    bool changePasswordSucceeded;
+                    try
+                    {
+                        changePasswordSucceeded = WebSecurity.ChangePassword(User.Identity.Name, model.OldPassword, model.NewPassword);
+                    }
+                    catch (Exception)
+                    {
+                        changePasswordSucceeded = false;
+                    }
+
+                    if (changePasswordSucceeded)
+                    {
+                        return RedirectToAction("Manage", new { Message = ManageMessageId.ChangePasswordSuccess });
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "La contraseña actual es incorrecta o la nueva contraseña no es válida.");
+                    }
+                }
+            }
+            else
+            {
+                // El usuario no dispone de contraseña local, por lo que debe quitar todos los errores de validación generados por un
+                // campo OldPassword
+                ModelState state = ModelState["OldPassword"];
+                if (state != null)
+                {
+                    state.Errors.Clear();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        WebSecurity.CreateAccount(User.Identity.Name, model.NewPassword);
+                        return RedirectToAction("Manage", new { Message = ManageMessageId.SetPasswordSuccess });
+                    }
+                    catch (Exception)
+                    {
+                        ModelState.AddModelError("", String.Format("No se puede crear una cuenta local. Es posible que ya exista una cuenta con el nombre \"{0}\".", User.Identity.Name));
+                    }
+                }
+            }
+
+            // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
+            return View(model);
+        }
+
+        #region Aplicaciones auxiliares
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            SetPasswordSuccess,
+            RemoveLoginSuccess,
+        }
+
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // Vaya a http://go.microsoft.com/fwlink/?LinkID=177550 para
+            // obtener una lista completa de códigos de estado.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "El nombre de usuario ya existe. Escriba un nombre de usuario diferente.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "Ya existe un nombre de usuario para esa dirección de correo electrónico. Escriba una dirección de correo electrónico diferente.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "La contraseña especificada no es válida. Escriba un valor de contraseña válido.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "La dirección de correo electrónico especificada no es válida. Compruebe el valor e inténtelo de nuevo.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "La respuesta de recuperación de la contraseña especificada no es válida. Compruebe el valor e inténtelo de nuevo.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "La pregunta de recuperación de la contraseña especificada no es válida. Compruebe el valor e inténtelo de nuevo.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "El nombre de usuario especificado no es válido. Compruebe el valor e inténtelo de nuevo.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "El proveedor de autenticación devolvió un error. Compruebe los datos especificados e inténtelo de nuevo. Si el problema continúa, póngase en contacto con el administrador del sistema.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "La solicitud de creación de usuario se ha cancelado. Compruebe los datos especificados e inténtelo de nuevo. Si el problema continúa, póngase en contacto con el administrador del sistema.";
+
+                default:
+                    return "Error desconocido. Compruebe los datos especificados e inténtelo de nuevo. Si el problema continúa, póngase en contacto con el administrador del sistema.";
+            }
+        }
+        #endregion
+    }
+}
